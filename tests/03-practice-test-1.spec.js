bbe6224c8e50faed8c9a706ec467dac01a7a6217
Phong Do <dominhphong306@gmail.com>
2024-11-27 17:35:06 +0700

    chore: add one more solution for test 1

--- tests/03-practice-test-1.spec.js
+++ tests/03-practice-test-1.spec.js
@@ -1,6 +1,14 @@
 const { test } = require('@playwright/test');
 
-test('Click test', async ({ page }) => {
+test('Click test - solution 01', async ({ page }) => {
+    await page.goto('https://material.playwrightvn.com/');
+    await page.click("//a[text()='Bài học 1: Register Page (có đủ các element)']");
+    await page.click("//a[text()='Trở về trang chủ']");
+    await page.click("//a[text()='Bài học 2: Product page']");
+    await page.click("//a[text()='Trở về trang chủ']");
+});
+
+test('Click test - solution 02', async ({ page }) => {
     await page.goto('https://material.playwrightvn.com/');
     await page.getByRole('link', { name: 'Bài học 1: Register Page (c' }).click();
     await page.getByRole('link', { name: 'Trở về trang chủ' }).click();
